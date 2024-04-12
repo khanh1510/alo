cace49a3d38f62111e099a4734171ab7f4f63d02
THƯƠNG HOÀI <khanh.tranquoc@hcmut.edu.vn>
2024-04-12 23:36:54 +0700

    messenger

--- app.py
+++ app.py
@@ -24,13 +24,18 @@ person = {"is_logged_in": False, "name": "", "email": "", "uid": ""}
 #End setup website
 
 #Login
+#/////////////////////////////////////////////////////
 @app.route("/")
 def login():
     if person["is_logged_in"] == True:
         return redirect(url_for('welcome'))
     else:
         return render_template("sign-in.html")
+#/////////////////////////////////////////////////////
     
+
+#/////////////////////////////////////////////////////
+
 #Home page
 @app.route("/welcome")
 def welcome():
@@ -38,8 +43,15 @@ def welcome():
         return render_template("index.html")
     else:
         return redirect(url_for('login'))
-    
+#/////////////////////////////////////////////////////
 
+
+
+
+
+
+#XỬ LÍ ĐĂNG NHẬP ĐĂNG XUẤT
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 #Sign up - Register
 @app.route("/signup")
 def signup():
@@ -58,6 +70,85 @@ def logout():
     person["name"] = ""
     return redirect(url_for('welcome'))
 
+
+#Xu li viec dang nhap co thanh cong hay khong
+@app.route("/result", methods=["POST", "GET"])
+def result():
+    if request.method == "POST":
+        result = request.form
+        email = result["email"]
+        password = result["pass"]
+        try:
+
+            user = auth.sign_in_with_email_and_password(email, password)
+
+            global person
+            person["is_logged_in"] = True
+            person["email"] = user["email"]
+            person["uid"] = user["localId"]
+
+            data = db.child("users").get()
+            person["name"] = data.val()[person["uid"]]["name"]
+
+            #set user session 
+            #session['user'] = person["email"]
+
+            return redirect(url_for('welcome'))
+        except:
+
+            return redirect(url_for('login'))
+    else:
+        if person['is_logged_in'] == True:
+            return redirect(url_for('welcome'))
+        else:
+            return redirect(url_for('login'))
+        
+#Xu li logic viec register
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        result = request.form
+        email = result["email"]
+        password = result["pass"]
+        name = result["name"]
+
+        try:
+            auth.create_user_with_email_and_password(email, password)
+
+            user = auth.sign_in_with_email_and_password(email, password)
+
+            global person
+            person["is_logged_in"] = True
+            person["email"] = user["email"]
+            person["name"] = name
+            person["uid"] = user["localId"]
+
+            data = {"name": name, "email": email}
+            db.child("users").child(person["uid"]).set(data)
+
+            #request.cookies.pop()
+            return redirect(url_for('welcome'))
+        except:
+            return redirect(url_for('register'))
+    else:
+        return redirect(url_for('welcome'))
+
+
+
+@app.after_request
+def add_nocache_headers(response):
+  response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+  response.headers['Pragma'] = 'no-cache'
+  response.headers['Expires'] = '0'
+  return response
+#/////////////////////////////////////////////////////
+
+
+
+
+
+
+#/////////////////////////////////////////////////////
 #Dashboard
 @app.route("/dashboard")
 def dashboard():
@@ -66,6 +157,21 @@ def dashboard():
     else:
         return redirect(url_for('welcome'))
     
+#Fiancial route
+@app.route("/fiancial")
+def fiancial():
+    if person["is_logged_in"] == True:
+        return render_template("fiancial.html")
+    else:
+        return redirect(url_for('welcome'))
+#/////////////////////////////////////////////////////
+
+
+
+
+
+#XỬ LÍ LIÊN QUAN ĐẾN BÁC SĨ
+#/////////////////////////////////////////////////////
 #Xu li de in ra ket qua cua nhan vien y bac si
 @app.route("/doctor")
 def process_logic():
@@ -87,15 +193,14 @@ def process_logic():
         return render_template("doctor.html", nhan_vien=nhan_vien)
     else:
         return redirect(url_for('welcome'))
+#/////////////////////////////////////////////////////
 
-#Fiancial route
-@app.route("/fiancial")
-def fiancial():
-    if person["is_logged_in"] == True:
-        return render_template("fiancial.html")
-    else:
-        return redirect(url_for('welcome'))
 
+
+
+
+#XỬ LÍ LIÊN QUAN ĐẾN THUỐC THANG
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 #THuoc uong route
 @app.route("/med/medicine_drink")
 def medicine_drink():
@@ -111,7 +216,13 @@ def medicine_tool():
         return render_template("medicine_tool.html")
     else:
         return redirect(url_for('welcome'))
-    
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+
+
+#XỬ LÍ LIÊN QUAN ĐẾN BỆNH NHÂN
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 #Dieu huong trang nhap du lieu benh nhan
 @app.route("/patient/patient_input")
 def patient_input():
@@ -201,78 +312,16 @@ def patient_ngoai():
         return render_template("patient_ngoai.html", danhsach=danhsach)
     else:
         return redirect(url_for('welcome'))
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-    
-#Xu li viec dang nhap co thanh cong hay khong
-@app.route("/result", methods=["POST", "GET"])
-def result():
-    if request.method == "POST":
-        result = request.form
-        email = result["email"]
-        password = result["pass"]
-        try:
-
-            user = auth.sign_in_with_email_and_password(email, password)
-
-            global person
-            person["is_logged_in"] = True
-            person["email"] = user["email"]
-            person["uid"] = user["localId"]
-
-            data = db.child("users").get()
-            person["name"] = data.val()[person["uid"]]["name"]
-
-            #set user session 
-            #session['user'] = person["email"]
-
-            return redirect(url_for('welcome'))
-        except:
-
-            return redirect(url_for('login'))
-    else:
-        if person['is_logged_in'] == True:
-            return redirect(url_for('welcome'))
-        else:
-            return redirect(url_for('login'))
-        
-#Xu li logic viec register
-@app.route("/register", methods=["POST", "GET"])
-def register():
-    if request.method == "POST":
-        result = request.form
-        email = result["email"]
-        password = result["pass"]
-        name = result["name"]
-
-        try:
-            auth.create_user_with_email_and_password(email, password)
-
-            user = auth.sign_in_with_email_and_password(email, password)
-
-            global person
-            person["is_logged_in"] = True
-            person["email"] = user["email"]
-            person["name"] = name
-            person["uid"] = user["localId"]
-
-            data = {"name": name, "email": email}
-            db.child("users").child(person["uid"]).set(data)
-
-            #request.cookies.pop()
-            return redirect(url_for('welcome'))
-        except:
-            return redirect(url_for('register'))
-    else:
-        return redirect(url_for('welcome'))
 
 
 
-@app.after_request
-def add_nocache_headers(response):
-  response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-  response.headers['Pragma'] = 'no-cache'
-  response.headers['Expires'] = '0'
-  return response
+    
+
+
+
+
 
 
 
